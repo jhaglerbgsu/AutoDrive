571fbf2bbd41dd278769960f92d92fdd1ae14886
fa20_djlamb <djlamb@bgsu.edu>
2021-07-14 00:25:07 -0400

    Most GoogLeNet errors fixed

--- Steering Angle Prediction/config.py
+++ Steering Angle Prediction/config.py
@@ -6,23 +6,22 @@ img_dir = "C:\\Users\\DJL57\\Documents\\dataset\\IMG"
 csv_src = "C:\\Users\\DJL57\\Documents\\dataset\\driving_log.csv"
 
 # target network
-# net = "TruckNN" 
 net = "GoogLeNet"
 #net = "TruckResnet18"
 
 # training
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-batch_size = 128 # 8 for LSTM
+batch_size = 32 # 128 for Resnet18 
 seq_len = 15 
 print_freq = 50
 tensorboard_freq = 50
-epochs = 20 #20 for normal 40 for resnet50 
+epochs = 20 
 lrate = 1e-4
 wdecay = 1e-4
 getLoss = torch.nn.MSELoss()
 train_test_split_ratio = 0.8
-early_stop_tolerance = 10 #4
+early_stop_tolerance = 10 
 fine_tune_ratio = 0.8
 is_continue = True
 
@@ -30,6 +29,8 @@ print_freq = 100
 tensorboard_freq = 200
 
 curtime = str(datetime.now())
+
+
 #ckpt_src = "./checkpoints/{1}/ckpt_{0}.pth".format(curtime.split(" ")[0] + "_" + 
 #            curtime.split(" ")[1][0:2] + "_" + curtime.split(" ")[1][3:5], net)
 #ckpt_src = "./checkpoints/{0}/best_ckpt.pth".format(net)
