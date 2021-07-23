e12ee22a6f90c7f9f5063b383a619e686416dc1b
fa20_djlamb <djlamb@bgsu.edu>
2021-07-23 10:53:01 -0400

    Code adjusted. All three models are implemented.

--- Steering Angle Prediction/inference.py
+++ Steering Angle Prediction/inference.py
@@ -5,8 +5,8 @@ import numpy as np
 from PIL import Image
 import cv2
 
-from models import TruckNN, TruckResnet18, GoogLeNet
-from config import device, best_ckpt_src, inf_img_src, inf_vid_src, inf_out_src, inf_out_img_src, inf_out_vid_src, net, seq_len
+from models import TruckResnet18, GoogLeNet
+from config import best_ckpt_src, inf_img_src, inf_vid_src, inf_out_src, inf_out_img_src, inf_out_vid_src, net, seq_len
 from utils import select_model, load_weights, preprocess_img
 from visualize import vis_angle_on_img
 
@@ -130,11 +130,7 @@ if __name__ == "__main__":
     # load model weights
     load_weights(model, best_ckpt_src, logger)
 
-    # inference
-    if net == "GoogLeNet":
-        inference_video_seq(model, logger)
-    else:
     # inference_image(model, logger)
-        inference_video(model, logger)
+    inference_video(model, logger)
     
     
